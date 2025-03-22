947e12400d7c52b7d403e06c15735ca72461fcf6
Lee Roberts <emailme@leeroberts.me>
2025-03-22 01:19:09 +0000

    Changed vars for $SQL_HOST and $SQL_PORT, which no longer exist.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status
+# Exit on error
 set -e
 
-# Wait for Postgres to be ready
+# Wait for PostgreSQL
 echo "Waiting for PostgreSQL to be available..."
-while ! nc -z $SQL_HOST $SQL_PORT; do
+while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
   sleep 1
 done
 
 echo "PostgreSQL is up. Running migrations..."
 
-# Run Django database migrations
+# Run migrations
 python manage.py migrate
 
 # Start the dev server
-python manage.py runserver 0.0.0.0:8000
+exec python manage.py runserver 0.0.0.0:8000
