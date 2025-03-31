4e262d7f16e82b8c2fa341f5e64966be22633a23
Lee Roberts (.Me) <gitme@leeroberts.me>
2025-03-31 23:24:15 +0100

    Update urls.py
    support for hello world! test and example added

--- django_config/urls.py
+++ django_config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views  # this works only if views.py is in the same folder
 
 urlpatterns = [
+    path('', views.hello),
     path('admin/', admin.site.urls),
 ]
