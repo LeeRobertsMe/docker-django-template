f3dc033b756ff4a982599cb174719e2ab15ae451
Lee Roberts (.Me) <gitme@leeroberts.me>
2025-04-01 00:03:18 +0100

    Update settings.py
    added missing "import os" by moving it up top

--- django_config/settings.py
+++ django_config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,7 +77,6 @@ WSGI_APPLICATION = 'django_config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-import os
 
 DATABASES = {
     'default': {
